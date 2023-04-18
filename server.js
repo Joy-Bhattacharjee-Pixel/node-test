099df0f4cb6a8e455e2e086a61fb13f1e9bd37a8
Joy Bhattacherjee <joy.bhattacharjee@pixelconsultancy.in>
2023-04-18 15:33:21 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+// This is required for parsing body from POST
+app.use(express.json());
+// Required for url encoding
+app.use(express.urlencoded({ extended: true }));
+// Importing db details
+
+// Dummy checking
+app.get("/test", (req, res) => {
+    res.json({ message: "Welcome to Inboxx Application" });
+});
+
+// Defining port
+const PORT = process.env.PORT || 8081;
+// Listenning to port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
